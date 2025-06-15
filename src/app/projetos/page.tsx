bafd3dea82cf272c511f4274c05e8512428ea008
Junior-81 <jab.junior81@gmail.com>
2025-06-15 19:24:27 -0300

    att link

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { ExternalLink, Github, Filter } from 'lucide-react';
 import { projects } from '@/data';
